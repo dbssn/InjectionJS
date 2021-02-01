91f7c7d33ab4d076beab1dd0a3f5e4ce9b01c3f7
dbssn <56835588+dbssn@users.noreply.github.com>
2021-02-01 13:04:47 +0100

    Update myScript.js

--- myScript.js
+++ myScript.js
@@ -1,7 +1,7 @@
 user = 'Pinocchio';
-if (typeof sessionStorage.tabID == 'undefined') {
-    tabID = user + Date.now().toString();
-    sessionStorage.setItem('tabID', tabID);
+if (typeof sessionStorage.tab_id == 'undefined') {
+    tab_id = user + Date.now().toString();
+    sessionStorage.setItem('tab_id', tab_id);
 } else {
     tab_id = sessionStorage.getItem('tab_id');
 }
