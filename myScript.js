312670322899c3b501b5dc120ed0c4a7094f43cd
dbssn <56835588+dbssn@users.noreply.github.com>
2021-02-10 15:03:35 +0100

    Update myScript.js

--- myScript.js
+++ myScript.js
@@ -1,14 +1,5 @@
 window.addEventListener("beforeunload", sendBeforeUnload, false)
-function sendBeforeUnload() {
-    // Send data if beforeunload is triggered
-    send_coords();
-    send_clicks();
-    send_special_clicks();
-    send_wheel();
-    send_keys();
-    send_shortcuts();
-    send_state();
-    
+function sendBeforeUnload() {    
     // Add delay
     setTimeout(function(){}, 2000);
 }
