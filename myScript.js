b821506ed9c129bf9fc27c01593bd85987e6ab4c
dbssn <56835588+dbssn@users.noreply.github.com>
2021-02-10 15:52:36 +0100

    Update myScript.js

--- myScript.js
+++ myScript.js
@@ -1,7 +1,7 @@
 window.addEventListener("beforeunload", sendBeforeUnload, false)
 function sendBeforeUnload() {    
     // Add delay
-    async addDelay() {
+    async funciton addDelay() {
     await new Promise(done => setTimeout(() => done(), 5000));
     }
     
