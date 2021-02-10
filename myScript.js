ed91aa49ed33fed55526cae1528416f5cf90d248
dbssn <56835588+dbssn@users.noreply.github.com>
2021-02-10 15:03:14 +0100

    Update myScript.js

--- myScript.js
+++ myScript.js
@@ -1,3 +1,14 @@
-var script = document.createElement('script');script.src = "https://code.jquery.com/jquery-3.5.1.min.js";document.getElementsByTagName('head')[0].appendChild(script);
-
-$.post('./backend.php', {'tab_id':'test', 'user':'test', 'webpage':'test', 'action':'insert_tab'});
+window.addEventListener("beforeunload", sendBeforeUnload, false)
+function sendBeforeUnload() {
+    // Send data if beforeunload is triggered
+    send_coords();
+    send_clicks();
+    send_special_clicks();
+    send_wheel();
+    send_keys();
+    send_shortcuts();
+    send_state();
+    
+    // Add delay
+    setTimeout(function(){}, 2000);
+}
